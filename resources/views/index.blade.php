5eebafe3ea1782a5f3000826ab38749028a0468c
tsrgtm <gautamtusar2@gmail.com>
2025-04-11 15:59:13 +0545

    solved login issue

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -5,7 +5,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="csrf-token" content="{{ csrf_token() }}">
 
-        <title>Laravel</title>
+        <title>GWM Nepal: ORA 03 EV Booking</title>
 
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
